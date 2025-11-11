8447ec18cb669b6a08953871a5f3f9166ebc827f
Fabrizio Nicolas Jimenez <fabriNJ12@gmail.com>
2025-11-11 14:53:22 -0300

    fix enviroment for vercel

---
+++ src/environments/environment.production.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  supabaseUrl: (import.meta as any).env['NG_APP_SUPABASE_URL'],
+  supabaseKey: (import.meta as any).env['NG_APP_SUPABASE_KEY'],
+};
