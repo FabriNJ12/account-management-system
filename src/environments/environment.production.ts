fa6aa6c11b39f0ca2bd8494d878969f1bbb9d3ec
Fabrizio Nicolas Jimenez <fabriNJ12@gmail.com>
2025-11-11 15:24:11 -0300

    another bug fix

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,5 +1,18 @@
+declare global {
+  interface ImportMetaEnv {
+    NG_APP_SUPABASE_URL: string;
+    NG_APP_SUPABASE_KEY: string;
+  }
+
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
+}
+
 export const environment = {
   production: true,
-  supabaseUrl: process.env['NG_APP_SUPABASE_URL'],
-  supabaseKey: process.env['NG_APP_SUPABASE_KEY'],
+  supabaseUrl: import.meta.env.NG_APP_SUPABASE_URL,
+  supabaseKey: import.meta.env.NG_APP_SUPABASE_KEY,
 };
+
+export {};
