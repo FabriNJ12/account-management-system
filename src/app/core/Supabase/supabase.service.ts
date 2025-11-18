b8316a19f13e0a9e4c3c11a24af733c680cb0e94
Fabrizio Nicolas Jimenez <fabriNJ12@gmail.com>
2025-11-17 21:40:19 -0300

    modified supabase service

--- src/app/core/Supabase/supabase.service.ts
+++ src/app/core/Supabase/supabase.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
-import { environment_local } from '../../../environments/environment.local';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -8,19 +8,19 @@ import { environment_local } from '../../../environments/environment.local';
 export class SupabaseService {
   private client: SupabaseClient;
 
+  constructor() {
+    this.client = createClient(
+      environment.supabaseUrl,
+      environment.supabaseKey
+    );
+  }
+
   // constructor() {
   //   this.client = createClient(
   //     environment.supabaseUrl,
   //     environment.supabaseKey
   //   );
   // }
-
-  constructor() {
-    this.client = createClient(
-      environment_local.supabaseUrl,
-      environment_local.supabaseKey
-    );
-  }
 
   GoTrueClient(): SupabaseClient {
     return this.client;
